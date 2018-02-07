71c4d49845cd14a1fbdd43520f9a718fe170102d
maramijade <35506019+maramijade@users.noreply.github.com>
2018-02-07 10:15:13 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,42 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # set the matrix holder
+  curMatrix <- NULL  # starts empty
+  # get the value of the matrix
+  place <- function(y)
+  {
+    x <<- y  # this sets the variable outside this context to be able to come back in later
+    curMatrix <<- NULL #initiates another holder that we want to be empty if we haven't created an inverse
+    
+  }
+  get <- function() x
+  # set the inverse holder
+  setinvhold <- function(inverse) curMatrix <<- inverse #allows for inverse to go outside this context
+  # get the value of the inverse
+  getinverse <- function() curMatrix
+  list (place= place,get = get,
+        setinvhold = setinvhold,
+        getinverse = getinverse)
+  
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## get the environment inverse
+  curMatrix <- x$getinverse()
+  ## check to see if it is null, if it is get the cached matrix
+  if(!is.null(curMatrix))
+  {
+    message( "getting cached data")
+    return(curMatrix)
+  }
+  
+  ## preform inverse
+  output <- x$get()
+  curMatrix <- solve(output, ...)
+  x$setinvhold(curMatrix)
+  curMatrix
 }
